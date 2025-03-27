15c8849ec3be40ceeb69f5c194d2b166d290f925
tyuvraj <tyuvraj77@gmail.com>
2025-03-27 19:53:21 +0530

    triangle gpu buffer

--- src/helpers.rs
+++ src/helpers.rs
@@ -58,6 +58,16 @@ impl <'a> IWgpuInit <'a> {
             sample_count,
         }
     }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
 }
 
 pub async fn get_wgpu_info(window: &Window) {
